d05f8bd386bb9c9572aba0a563bc65049bb143c7
bitspilanigroup2 <bitspilanigroup2@gmail.com>
2021-02-25 18:30:19 +0530

    included few columns for training

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,19 @@ import configparser
 import os
 import pandas as pd
 import numpy as np
+from sklearn.impute import SimpleImputer
+
+def imputeByMean(data, nonNumeric):
+    imputedMean = SimpleImputer(strategy='mean') # for median imputation replace 'mean' with 'median'
+    dataNumeric = data.drop(nonNumeric, axis=1)
+    imputedMean.fit(dataNumeric)
+    imputedData = imputedMean.transform(dataNumeric)
+
+    d1 = pd.DataFrame(imputedData)
+    d1.columns = dataNumeric.columns
+    finalImputedData = pd.concat([d1, data.loc[:,nonNumeric]], axis=1)
+    ## imputedData.columns = data.columns
+    return finalImputedData
 
 
 def make_datasets():
@@ -13,13 +26,32 @@ def make_datasets():
     config.read(os.getcwd() + '/tox.ini')
     df = pd.read_csv(config['data']['train-dataset-path'])
     print(df.head(3))
+    # df["education"].replace(np.nan, df["education"].astype("float32").mean(axis = 0), inplace = True)
+    # df["cigsPerDay"].replace(np.nan, df["cigsPerDay"].astype("float32").mean(axis = 0), inplace = True)
+    # df["BPMeds"].replace(np.nan, df["BPMeds"].astype("float32").mean(axis = 0), inplace = True)
+    # df["totChol"].replace(np.nan, df["totChol"].astype("float32").mean(axis = 0), inplace = True)
+    # df["BMI"].replace(np.nan, df["BMI"].astype("float32").mean(axis = 0), inplace = True)
+    # df["glucose"].replace(np.nan, df["glucose"].astype("float32").mean(axis = 0), inplace = True)
+    cols = df.columns
+    num_cols = df._get_numeric_data().columns
+    nonNumeric=list(set(cols) - set(num_cols))
+    df = imputeByMean(df,nonNumeric)
+    # df["education"].replace(np.nan, df["education"].astype("float32").mean(axis = 0), inplace = True)
+    df[['education', 'cigsPerDay', 'BPMeds', 'totChol', 'sysBP', 'diaBP', 'BMI', 'glucose']] = df[['education', 'cigsPerDay', 'BPMeds', 'totChol', 'sysBP', 'diaBP', 'BMI', 'glucose']].astype(int)
     df["education"].replace(np.nan, df["education"].astype("float32").mean(axis = 0), inplace = True)
+    df["cigsPerDay"].replace(np.nan, df["cigsPerDay"].astype("float32").mean(axis = 0), inplace = True)
+    df["BPMeds"].replace(np.nan, df["BPMeds"].astype("float32").mean(axis = 0), inplace = True)
+    df["totChol"].replace(np.nan, df["totChol"].astype("float32").mean(axis = 0), inplace = True)
+    df["BMI"].replace(np.nan, df["BMI"].astype("float32").mean(axis = 0), inplace = True)
+    df["glucose"].replace(np.nan, df["glucose"].astype("float32").mean(axis = 0), inplace = True)
     data = dict()
     input_columns = ['male', 'age', 'education', 'currentSmoker', 'cigsPerDay', 'BPMeds',
        'prevalentStroke', 'prevalentHyp', 'diabetes', 'totChol', 'sysBP',
        'diaBP', 'BMI', 'heartRate', 'glucose']
     # have to include all columns, for now adding few columns
-    input_columns = ['male','age', 'education', 'currentSmoker']
+    input_columns = ['male','age', 'education', 'currentSmoker', 'cigsPerDay', 'BPMeds',
+    # 'prevalentStroke', 'prevalentHyp', 'diabetes', 'totChol', 'sysBP',
+    'diaBP', 'BMI', 'heartRate', 'glucose']
     output_column = 'TenYearCHD'
     data['X'] = df[input_columns]
     data['Y'] = df[output_column]
